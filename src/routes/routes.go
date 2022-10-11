9416941ae5e3cfceab418cd4c18d2cb12a4106c0
JAsuquo <apple@APPLEs-MacBook-Pro.local>
2022-10-11 16:34:59 +0100

    user-microservice completed

---
+++ src/routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"users/src/controllers"
+	"users/src/middlewares"
+)
+
+func Setup(app *fiber.App) {
+	api := app.Group("api")
+
+	api.Post("register", controllers.Register)
+	api.Post("login", controllers.Login)
+	api.Get("users", controllers.Users)
+	api.Get("users/:id", controllers.GetUser)
+
+	authenticated := api.Use(middlewares.IsAuthenticated)
+	authenticated.Get("user/:scope", controllers.User)
+	authenticated.Post("logout", controllers.Logout)
+	authenticated.Put("users/info", controllers.UpdateInfo)
+	authenticated.Put("users/password", controllers.UpdatePassword)
+
+}
